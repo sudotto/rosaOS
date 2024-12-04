d6dfed28fbb273ed43587dd8b693787841570e74
sudotto <codekiwi132@gmail.com>
2024-12-03 20:42:31 -0800

    i just implemented a gdt and entered protected mode in less than an hour i think i'm getting smarter

--- src/boot.asm
+++ src/boot.asm
@@ -1,5 +1,23 @@
 bits 32
-section .text
+section .data                                              ; data section
+
+gdt_start:
+	dd 0x00000000                                          ; null limit
+	dd 0x00000000                                          ; null base and access
+	
+	dd 0x0000FFFF                                          ; text limit
+	dd 0x00CF9A00                                          ; text base and access
+	
+	dd 0x0000FFFF                                          ; data limit
+	dd 0x00CF9200                                          ; data base and access
+
+gdt_end:
+
+gdt:
+	dw gdt_end - gdt_start - 1                             ; size of gdt
+	dd gdt_start                                           ; location of the gdt 
+
+section .text                                              ; text/code section
 	align 4
 	dd 0x1badb002                                          ; magic number
 	dd 0x00                                                ; flags
@@ -10,5 +28,11 @@ global start
 
 start:
 	cli                                                    ; stop interrupts
-	call main                                             ; call the kernel
+	lgdt [gdt]                                             ; load gdt
+	mov ax, 0x08
+	mov ds, ax
+	mov eax, cr0                                           ; put cr0 into eax
+	or eax, 1                                              ; enable protected mode bit
+	mov cr0, eax                                           ; put it back into cr0
+	call main                                              ; call the kernel
 	hlt                                                    ; halt CPU
